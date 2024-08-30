cdcc1311945d832b99c68315760f77440359cccf
Young Yang <jyang@stust.edu.tw>
2024-08-30 12:57:40 +0800

    home

---
+++ week01/lab02/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int count;
+    cin >> count;
+    for(int i = 0; i < count; i++) {
+        cout << "Hello, STUST!" << endl;
+    }
+
+    return 0;
+}
